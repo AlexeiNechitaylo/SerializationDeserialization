37ec6483e3c2432a0f07a41c3b7384773c986b8c
NeAlexei <a.nechitaylo@hypervsn.com>
2020-01-09 16:57:42 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+import serdes_1.User;
+
+import com.google.gson.Gson;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        serializeUser();
+        deserializeUser();
+    }
+
+        private static void serializeUser() {
+            User user = new User(25, "Smith", "Smith@gmail.com");
+
+            Gson gson = new Gson();
+            String json = gson.toJson(user);
+            System.out.println(json);
+        }
+
+        private static void deserializeUser(){
+            String userJson = "{'age': 30, 'name': 'Valera', 'email': 'Valera@gmail.com'}";
+
+            Gson gson = new Gson();
+            gson.fromJson(userJson, User.class);
+
+        }
+}
+
---
+++ src/serdes_1/User.java
@@ -0,0 +1,39 @@
+package serdes_1;
+
+public class User {
+    private int age;
+    private String name;
+    private String email;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public User(int age, String name, String email){
+        this.age = age;
+        this.name = name;
+        this.email = email;
+
+
+    }
+}
